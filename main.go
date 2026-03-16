4f33560df382e095a49f0e7980df308685865349
Himanshu Sardana <himanshusardana2005@gmail.com>
2026-03-16 18:28:53 +0530

    feat: add basic markdown parsing

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/gomarkdown/markdown"
+	// "github.com/gomarkdown/markdown/html"
+	// "github.com/gomarkdown/markdown/parser"
+)
 
 func main() {
-	fmt.Println("testing")
+	path := filepath.Join("./test.md")
+	mds, err := os.ReadFile(path)
+	if err != nil {
+		fmt.Println("Error %s", err)
+	}
+	md := []byte(mds)
+	html := markdown.ToHTML(md, nil, nil)
+
+	fmt.Printf("--- Markdown:\n%s\n\n--- HTML:\n%s\n", md, html)
 }
